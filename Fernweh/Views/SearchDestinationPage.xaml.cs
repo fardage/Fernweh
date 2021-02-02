ffb0ce5673145beec26e16ed3d22911f595a9db5
Marvin <marvin@tseng.ch>
2021-02-02 17:13:03 +0100

    android ui adjustments

---
+++ Fernweh/Views/SearchDestinationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Fernweh.Views
+{
+    public partial class SearchDestinationPage : ContentPage
+    {
+        public SearchDestinationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
