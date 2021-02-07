0bfe861dba6c761277517d8ec88740a7332b0d05
Marvin <marvin@tseng.ch>
2021-02-07 09:26:05 +0100

    device orientation portrait only

--- Fernweh.Android/MainActivity.cs
+++ Fernweh.Android/MainActivity.cs
@@ -9,8 +9,13 @@ using Platform = Xamarin.Essentials.Platform;
 namespace Fernweh.Droid
 {
     [Activity(Label = "Fernweh", Icon = "@mipmap/icon", Theme = "@style/MainTheme",
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
-                               ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+        ConfigurationChanges = ConfigChanges.ScreenSize
+        | ConfigChanges.UiMode
+        | ConfigChanges.ScreenLayout
+        | ConfigChanges.SmallestScreenSize
+        | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+
+
     public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
