c93f801731ba71055509550b7fab5d3919135c52
Marvin <marvin@tseng.ch>
2021-02-10 19:07:48 +0100

    edit sed command

--- Fernweh.Android/appcenter-pre-build.sh
+++ Fernweh.Android/appcenter-pre-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i -e "s/HEREMAPS_API_KEY/$HEREMAPS_API_KEY/g" Fernweh/Services/Credentials.cs
-sed -i -e "s/APPCENTER_API_KEY/$APPCENTER_API_KEY/g" Fernweh/Services/Credentials.cs
+sed -i "" -e "s/HEREMAPS_API_KEY/$HEREMAPS_API_KEY/g" Fernweh/Services/Credentials.cs
+sed -i "" -e "s/APPCENTER_API_KEY/$APPCENTER_API_KEY/g" Fernweh/Services/Credentials.cs
