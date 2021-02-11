ec5727b8c6238600b692ee0b9a8f4a87ac60745e
fardage <48010396+fardage@users.noreply.github.com>
2021-02-11 09:05:14 +0100

    Update Credentials.cs

--- Fernweh/Services/Credentials.cs
+++ Fernweh/Services/Credentials.cs
@@ -2,7 +2,7 @@ namespace Fernweh.Services
 {
     public static class Credentials
     {
-        public const string ApiKeyHere = "HEREMAPS_API_KEY";
+        public const string ApiKeyHereMaps = "HEREMAPS_API_KEY";
         public const string ApiKeyAppCenter = "APPCENTER_API_KEY";
     }
 }
